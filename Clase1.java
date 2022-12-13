900389f387fca036e6208ba530a0aaaf84227d93
dsarotti <sr.sarotti@gmail.com>
2022-12-13 16:02:04 +0100

    crear variable br1

--- Clase1.java
+++ Clase1.java
@@ -2,5 +2,6 @@ public class Clase1 {
     String mensaje = "Primer commit";
     public static void main(String[] args) {
         //método en construcción.
+        String br1="rama br1";
     }
 }
